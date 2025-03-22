89688f12fab1c2efdeafbfce5778fe4a43d3fd32
EndicottRobotics <blucas@endicott.edu>
2025-03-22 14:08:45 -0400

    it compiled

--- build/endicott_interfaces/rosidl_typesupport_fastrtps_cpp/endicott_interfaces/msg/detail/dds_fastrtps/move_status__type_support.cpp
+++ build/endicott_interfaces/rosidl_typesupport_fastrtps_cpp/endicott_interfaces/msg/detail/dds_fastrtps/move_status__type_support.cpp
@@ -16,6 +16,30 @@
 
 
 // forward declaration of message dependencies and their conversion functions
+namespace std_msgs
+{
+namespace msg
+{
+namespace typesupport_fastrtps_cpp
+{
+bool cdr_serialize(
+  const std_msgs::msg::Header &,
+  eprosima::fastcdr::Cdr &);
+bool cdr_deserialize(
+  eprosima::fastcdr::Cdr &,
+  std_msgs::msg::Header &);
+size_t get_serialized_size(
+  const std_msgs::msg::Header &,
+  size_t current_alignment);
+size_t
+max_serialized_size_Header(
+  bool & full_bounded,
+  bool & is_plain,
+  size_t current_alignment);
+}  // namespace typesupport_fastrtps_cpp
+}  // namespace msg
+}  // namespace std_msgs
+
 
 namespace endicott_interfaces
 {
@@ -32,6 +56,10 @@ cdr_serialize(
   const endicott_interfaces::msg::MoveStatus & ros_message,
   eprosima::fastcdr::Cdr & cdr)
 {
+  // Member: header
+  std_msgs::msg::typesupport_fastrtps_cpp::cdr_serialize(
+    ros_message.header,
+    cdr);
   // Member: state
   cdr << ros_message.state;
   // Member: distance_error
@@ -47,6 +75,10 @@ cdr_deserialize(
   eprosima::fastcdr::Cdr & cdr,
   endicott_interfaces::msg::MoveStatus & ros_message)
 {
+  // Member: header
+  std_msgs::msg::typesupport_fastrtps_cpp::cdr_deserialize(
+    cdr, ros_message.header);
+
   // Member: state
   cdr >> ros_message.state;
 
@@ -72,6 +104,11 @@ get_serialized_size(
   (void)padding;
   (void)wchar_size;
 
+  // Member: header
+
+  current_alignment +=
+    std_msgs::msg::typesupport_fastrtps_cpp::get_serialized_size(
+    ros_message.header, current_alignment);
   // Member: state
   current_alignment += padding +
     eprosima::fastcdr::Cdr::alignment(current_alignment, padding) +
@@ -111,6 +148,25 @@ max_serialized_size_MoveStatus(
   full_bounded = true;
   is_plain = true;
 
+
+  // Member: header
+  {
+    size_t array_size = 1;
+
+
+    last_member_size = 0;
+    for (size_t index = 0; index < array_size; ++index) {
+      bool inner_full_bounded;
+      bool inner_is_plain;
+      size_t inner_size =
+        std_msgs::msg::typesupport_fastrtps_cpp::max_serialized_size_Header(
+        inner_full_bounded, inner_is_plain, current_alignment);
+      last_member_size += inner_size;
+      current_alignment += inner_size;
+      full_bounded &= inner_full_bounded;
+      is_plain &= inner_is_plain;
+    }
+  }
 
   // Member: state
   {
